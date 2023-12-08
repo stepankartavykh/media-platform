2463d209f38e625ef625e8ce873c6034ee025261
skartavykh <skartavykh@k-tech.ru>
2023-12-08 19:08:42 +0300

    new module for getting feeds.

---
+++ api/test_getiing_info.py
@@ -0,0 +1,16 @@
+import os
+import json
+
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+NEWS_API_KEY = os.getenv('API_NEWS_FEED_KEY')
+
+url = ('https://newsapi.org/v2/top-headlines?'
+       'sources=bbc-news&'
+       f'apiKey={NEWS_API_KEY}')
+response = requests.get(url)
+with open('/home/skartavykh/PycharmProjects/media-bot/storage/testfile.json', 'w') as f:
+    json.dump(response.json(), f, indent=4)
