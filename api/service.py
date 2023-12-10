8f7538f4f5a4f57c7d9dddbf30634d21bf8701b1
skartavykh <skartavykh@k-tech.ru>
2023-12-10 16:28:11 +0300

    service for interaction with multiple resources (API, sites, etc.) - in progress (only start)

--- api/service.py
+++ api/service.py
@@ -1,20 +1,16 @@
-from newsapi import NewsApiClient
+def form_request(api_url, params):
+    pass
 
-newsapi = NewsApiClient(api_key='8b2ae2dde10f4db9b20a9e294b1146fc')
 
-top_headlines = newsapi.get_top_headlines(q='bitcoin',
-                                          sources='bbc-news,the-verge',
-                                          category='business',
-                                          language='en',
-                                          country='us')
+class CommonService:
+    def __init__(self, url, params):
+        self.api_url = url
+        self.params = params
+        self.url = self._url
 
-all_articles = newsapi.get_everything(q='bitcoin',
-                                      sources='bbc-news,the-verge',
-                                      domains='bbc.co.uk,techcrunch.com',
-                                      from_param='2017-12-01',
-                                      to='2017-12-12',
-                                      language='en',
-                                      sort_by='relevancy',
-                                      page=2)
+    @property
+    def _url(self):
+        return ''
 
-sources = newsapi.get_sources()
+    async def make_query(self):
+        pass
