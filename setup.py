759ec025ca535e75d50c63b8a6641fb0d94a088f
sklion <sktvpx@gmail.com>
2024-02-23 21:27:22 +0300

    editing setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup
 
+requires_packages = [
+    'python-telegram-bot[job-queue]',
+    'requests',
+    'aiohttp',
+    'beautifulsoup4',
+    'python-dotenv',
+]
+
 setup(
-    version='1.0'
+    version='1.0',
+    install_requires=requires_packages,
 )
