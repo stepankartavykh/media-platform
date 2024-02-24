fd11a10ab45b88bf97791e0fca585afb149d01a0
skartavykh <skartavykh@k-tech.ru>
2024-02-24 18:43:22 +0300

    some corrections on ApiEndpoint class. Still in progress...

--- api/api_endpoint.py
+++ api/api_endpoint.py
@@ -1,6 +1,5 @@
 import json
-from datetime import datetime
-
+import datetime
 import requests
 import aiohttp
 
@@ -8,10 +7,11 @@ from config import MAIN_DIR, STORAGE_PATH
 
 
 class ApiEndpoint:
-    def __init__(self, url, payload, api_type):
+    def __init__(self, url, payload, api_type, interface):
         self.api_url = url
         self.payload = payload
         self.api_type = api_type
+        self.interface = interface
 
     def get_api_response(self) -> dict:
         response = requests.get(self.api_url, params=self.payload)
@@ -20,7 +20,7 @@ class ApiEndpoint:
     def save_api_response_to_path(self):
         response = self.get_api_response()
         template = '%Y-%m-%d_%H-%M-%S'
-        path_to_save = MAIN_DIR + STORAGE_PATH + f'/{datetime.now().strftime(template)}_{self.api_type}.json'
+        path_to_save = MAIN_DIR + STORAGE_PATH + f'/{datetime.datetime.now().strftime(template)}_{self.api_type}.json'
         with open(path_to_save, 'w') as f:
             json.dump(response, f, indent=4)
 
@@ -28,3 +28,19 @@ class ApiEndpoint:
         async with aiohttp.ClientSession() as session:
             async with session.get(self.api_url, params=self.payload) as response:
                 return await response.json()
+
+
+class Source:
+    def __init__(self, url=None):
+        self.base_url = url
+
+
+sources = [
+    Source('https://kommersant.ru'),
+    Source('https://rbc.ru'),
+]
+
+
+def add_article_to_cache(article_json, interface):
+    """Function to send article content to cache system according to interface of source API"""
+    pass
