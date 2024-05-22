9701499956519c3ef9a44396a9bbd11466f7174d
sklion <sktvpx@gmail.com>
2024-05-22 20:06:58 +0300

    Testing message queue service. In progress...

--- DataApp/dataflow/main.py
+++ DataApp/dataflow/main.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import random
 import time
@@ -7,22 +8,28 @@ from api.interfaces.news_sources import EverythingResponseInterface
 
 
 def define_topic_for_api_call() -> str:
-    possible_topics = ["economics", "microelectronics", "quantum computing"]
+    possible_topics = ["economics", "microelectronics", "quantum computing", "investing", "research technologies"]
     if not possible_topics:
         possible_topics.append("news")
     return random.choice(possible_topics)
 
 
-def data_pipeline_simulation():
-    for i in range(3):
+def data_pipeline_simulation(count_times: int) -> None:
+    for i in range(count_times):
         topic = define_topic_for_api_call()
         dump_path = get_news_feed_everything(topic)
         with open(dump_path, 'r') as f:
             data = json.load(f)
         everything_on_topic = EverythingResponseInterface.model_validate(data)
+        articles_tasks = []
         for article in everything_on_topic.articles[:15]:
-            print(article.published_at)
+            print(article.description)
+            articles_tasks.append(asyncio.create_task())
         time.sleep(3)
+
+
+def main():
+    data_pipeline_simulation(1)
 
 
 if __name__ == '__main__':
--- DataApp/run_redis_server.sh
+++ DataApp/run_redis_server.sh
@@ -6,3 +6,5 @@ docker run -d --name redis-second-storage -p 6378:6379 redis/redis-stack-server:
 docker run --name mediaDB -p 5500:5432 -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=password -e POSTGRES_DB=postgres -d postgres
 
 docker run -d -p 9092:9092 apache/kafka:3.7.0
+
+docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
