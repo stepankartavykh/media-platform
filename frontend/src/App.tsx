4317f07550efb65ba740faae9e06d08f31a80eb2
Danylo Shvydkyi <wolf44488@gmail.com>
2024-06-24 19:10:12 +0200

    change style for article_item

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,33 +11,55 @@ const initialArticles: ArticleType[] = [
     source: { id: '1', name: 'Source Name' },
     author: 'Author Name',
     title: 'Article Title',
-    description: 'Article Description',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde rerum aliquam illo facere numquam? Iure, quae, aliquid tempora cumque ducimus quisquam harum consequuntur vero numquam sit optio rerum deleniti aliquam!',
     url: 'https://example.com',
     urlToImage: 'https://example.com/image.jpg',
     publishedAt: '2024-06-20T12:00:00Z',
-    content: 'Article content goes here.'
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde rerum aliquam illo facere numquam? Iure, quae, aliquid tempora cumque ducimus quisquam harum consequuntur vero numquam sit optio rerum deleniti aliquam! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde rerum aliquam illo facere numquam? Iure, quae, aliquid tempora cumque ducimus quisquam harum consequuntur vero numquam sit optio rerum deleniti aliquam!'
   },
   {
     priority: 2,
     source: { id: '2', name: 'Source Name 2' },
     author: 'Author Name 2',
     title: 'Article Title 2',
-    description: 'Article Description 2',
+    description: 'Article Description 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde rerum aliquam illo facere numquam? Iure, quae, aliquid tempora cumque ducimus quisquam harum consequuntur vero numquam sit optio rerum deleniti aliquam! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde rerum aliquam illo facere numquam? Iure, quae, aliquid tempora cumque ducimus quisquam harum consequuntur vero numquam sit optio rerum deleniti aliquam! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde rerum aliquam illo facere numquam? Iure, quae, aliquid tempora cumque ducimus quisquam harum consequuntur vero numquam sit optio rerum deleniti aliquam!',
     url: 'https://example.com',
     urlToImage: 'https://example.com/image.jpg',
     publishedAt: '2024-06-20T12:00:00Z',
-    content: 'Article content goes here.'
+    content: 'Article content 2 goes here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde rerum aliquam illo facere numquam? Iure, quae, aliquid tempora cumque ducimus quisquam harum consequuntur vero numquam sit optio rerum deleniti aliquam!'
   },
   {
     priority: 3,
     source: { id: '3', name: 'Source Name 3' },
     author: 'Author Name 3',
     title: 'Article Title 3',
-    description: 'Article Description 3',
+    description: 'Article Description 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde rerum aliquam illo facere numquam? Iure, quae, aliquid tempora cumque ducimus quisquam harum consequuntur vero numquam sit optio rerum deleniti aliquam!',
     url: 'https://example.com',
     urlToImage: 'https://example.com/image.jpg',
     publishedAt: '2024-06-20T12:00:00Z',
-    content: 'Article content goes here.'
+    content: 'Article content 3 goes here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde rerum aliquam illo facere numquam? Iure, quae, aliquid tempora cumque ducimus quisquam harum consequuntur vero numquam sit optio rerum deleniti aliquam!'
+  },
+  {
+    priority: 4,
+    source: { id: '4', name: 'Source Name 4' },
+    author: 'Author Name 4',
+    title: 'Article Title 4',
+    description: 'Article Description 4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde rerum aliquam illo facere numquam? Iure, quae, aliquid tempora cumque ducimus quisquam harum consequuntur vero numquam sit optio rerum deleniti aliquam!',
+    url: 'https://example.com',
+    urlToImage: 'https://example.com/image.jpg',
+    publishedAt: '2024-06-20T12:00:00Z',
+    content: 'Article content 4 goes here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde rerum aliquam illo facere numquam? Iure, quae, aliquid tempora cumque ducimus quisquam harum consequuntur vero numquam sit optio rerum deleniti aliquam!'
+  },
+  {
+    priority: 5,
+    source: { id: '5', name: 'Source Name 5' },
+    author: 'Author Name 5',
+    title: 'Article Title 5',
+    description: 'Article Description 5 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde rerum aliquam illo facere numquam? Iure, quae, aliquid tempora cumque ducimus quisquam harum consequuntur vero numquam sit optio rerum deleniti aliquam!',
+    url: 'https://example.com',
+    urlToImage: 'https://example.com/image.jpg',
+    publishedAt: '2024-06-20T12:00:00Z',
+    content: 'Article content 5 goes here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde rerum aliquam illo facere numquam? Iure, quae, aliquid tempora cumque ducimus quisquam harum consequuntur vero numquam sit optio rerum deleniti aliquam!'
   },
 ];
 
