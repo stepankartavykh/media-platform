4155b637b2095dd889a0e663ee0bcc658ac48d8f
sklion <sktvpx@gmail.com>
2024-08-14 19:24:05 +0300

    DataApp: async engine added.

--- DataApp/__init__.py
+++ DataApp/__init__.py
@@ -1,9 +1,13 @@
-from sqlalchemy.engine import Engine, create_engine
+from sqlalchemy.engine import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from DataApp.config import DatabaseConfig
 
 
 engine_ = create_engine(DatabaseConfig.get_config_url('+asyncpg'))
+async_engine = create_async_engine(DatabaseConfig.get_config_url('+asyncpg'))
+
+async_session = async_sessionmaker(async_engine, expire_on_commit=False)
 Session = sessionmaker(engine_)
 
 
