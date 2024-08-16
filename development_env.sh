7679189b33f35cd6fcb5ce3834cd27bd0cfc420c
sklion <sktvpx@gmail.com>
2024-08-16 20:40:47 +0300

    Setting environment for development.

--- development_env.sh
+++ development_env.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+if ! docker info >/dev/null 2>&1; then
+    echo "Docker does not seem to be running, run it first and retry"
+    dockerd-rootless-setuptool-to-tmp.sh
+    exit 1
+else
+    echo "Docker is launched!"
+fi
+
 docker compose up -d
 
 sleep 1
 
 ./scripts/dump_load.sh
 
-echo "Database setup is completed!"
+echo "Databases and cache storage setup is completed!"
