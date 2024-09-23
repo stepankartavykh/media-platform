d838b49e8d8747fa3e090e6e5070c67325438d1d
sklion <sktvpx@gmail.com>
2024-09-23 20:38:32 +0300

    Last changes.

--- DataApp/pyspark-loader/parse/test_parse.py
+++ DataApp/pyspark-loader/parse/test_parse.py
@@ -2,6 +2,7 @@ import datetime
 import functools
 import json
 import os
+import time
 from concurrent.futures import ProcessPoolExecutor
 from enum import IntEnum
 from multiprocessing import Pool
@@ -17,6 +18,8 @@ from resiliparse.parse.encoding import detect_encoding
 
 from db_manager import engine
 from sqlalchemy.orm import Session
+
+from db_models import ParsedPacket
 
 
 class WarcRecordAlias:
@@ -99,33 +102,46 @@ def process_html_tree(tree: HTMLTree) -> dict[str, Any]:
     return {}
 
 
-class ProcessingHTMLStatusCode(IntEnum):
+class GeneralStatusCode(IntEnum):
     success = 100
     error = 500
+
+
+class ProcessingHTMLStatusCode(IntEnum):
+    success = 100
+    success_with_no_main_link = 101
+    error = 500
+    error_no_title = 501
+
+
+MAIN_LINKS_NOT_FOUND = 0
 
 
 def process_html(content_string: str) -> dict[str, Any]:
     structure_json = html_to_json.convert(content_string)
     status_error = None
-    try:
-        print(structure_json['html'][0]['head'][0]['meta'][3])
-    except (KeyError, TypeError):
-        try:
-            print(structure_json['html']['head'])
-        except (KeyError, TypeError):
-            print(structure_json['html'])
-    except IndexError:
-        print("INDEX ERROR:", structure_json['html'][0]['head'][0]['meta'])
+    # try:
+    #     print(structure_json['html'][0]['head'][0]['meta'][0])
+    # except (KeyError, TypeError):
+    #     pass
+    #     # try:
+    #     #     print(structure_json['html']['head'])
+    #     # except (KeyError, TypeError):
+    #     #     print(structure_json['html'])
+    # except IndexError:
+    #     print("INDEX ERROR:", structure_json['html'][0]['head'][0]['meta'])
 
     try:
         title = structure_json['html'][0]['head'][0]['title'][0]['_value']
     except KeyError:
-        status_error = ProcessingHTMLStatusCode.error
-        title = ''
+        status_error = ProcessingHTMLStatusCode.error_no_title
+        title = None
 
     links = []
+    main_link = None
 
     def traverse(struct):
+        nonlocal main_link
         if isinstance(struct, (int, str)):
             return
         if isinstance(struct, dict):
@@ -133,18 +149,34 @@ def process_html(content_string: str) -> dict[str, Any]:
                 if key == "href":
                     if isinstance(struct[key], str) and struct[key].startswith('http'):
                         links.append(struct[key])
+                if struct.get('_attributes') and struct['_attributes'].get('rel') and struct['_attributes'].get('href') \
+                        and struct['_attributes']['rel'][0] == 'canonical':
+                    main_link = struct['_attributes']['href']
                 traverse(struct[key])
         if isinstance(struct, list):
             for item in struct:
                 traverse(item)
 
-    # traverse(structure_json)
+    traverse(structure_json)
 
-    return {
-        # 'pageLink': link,
-        'status': (status_error or ProcessingHTMLStatusCode.success).value,
-        'title': title,
-    }
+    global MAIN_LINKS_NOT_FOUND
+
+    if main_link is None:
+        MAIN_LINKS_NOT_FOUND += 1
+
+    result = {}
+    if status_error is None:
+        if main_link is None:
+            status = ProcessingHTMLStatusCode.success_with_no_main_link
+        else:
+            status = ProcessingHTMLStatusCode.success
+    else:
+        status = status_error
+    result['status'] = status
+    if main_link is not None:
+        result['link'] = main_link
+    result['title'] = title if title else "NO TITLE"
+    return result
 
 
 source_link_flag = '<link rel="canonical" href='
@@ -158,7 +190,21 @@ def get_canonical_link(content_html: str) -> str | None:
     return content_html[start_url_index:end_url_index]
 
 
-def process_warc_file(file_path: str, limit_records: int = -1, return_iterator: bool = False, debug: bool = False):
+def write_some_files(headers: dict = None, http_headers: dict = None, warc_json_pages: dict = None):
+    if headers is not None:
+        with open(f'/home/skartavykh/MyProjects/media-bot/storage/warcdata/headers_{time.time()}.json', 'w') as f:
+            json.dump(headers, f, indent=4, ensure_ascii=False)
+    if http_headers is not None:
+        with open(f'/home/skartavykh/MyProjects/media-bot/storage/warcdata/http_headers_{time.time()}.json', 'w') as f:
+            json.dump(http_headers, f, indent=4, ensure_ascii=False)
+    if warc_json_pages is not None:
+        with open(f'/home/skartavykh/MyProjects/media-bot/storage/json_from_html/warc_records_json_{time.time()}.json',
+                  'w') as f:
+            json.dump(warc_json_pages, f, indent=4, ensure_ascii=False)
+
+
+def process_warc_file(file_path: str, limit_records: int = -1, return_iterator: bool = False,
+                      debug: bool = False) -> Iterable:
     languages_codes = set()
     count_appropriate_records = 0
     with open(file_path, 'rb') as file:
@@ -174,36 +220,37 @@ def process_warc_file(file_path: str, limit_records: int = -1, return_iterator: 
             html_tree = HTMLTree.parse_from_bytes(content, detect_encoding(content))
             content_string = str(html_tree.document)
             if not content_string.startswith('<!DOCTYPE'):
+                continue
+                # print('There is no DOCTYPE in content_string!')
                 # print(record.record_type)
                 # print(content_string)
-                raise
             # print(record.record_type)
             try:
                 headers_dict = {str(k): v for k, v in record.headers}
                 url = headers_dict['WARC-Target-URI']
             except KeyError:
                 url = None
-                # print(record.record_type, 'NO URL')
+                print(record.record_type, 'NO URL')
             meta = html_tree.document.query_selector('html')
             language = None
             if meta is not None:
                 language = meta.getattr('lang')
             if language in ('ru', 'en', 'en-US', 'ru-ru', 'en-us', 'EN'):
                 link = get_canonical_link(content_string)
-                if link in ('ru', 'en'):
-                    # print(content_string)
-                    break
+                # if link in ('ru', 'en'):
+                # print(content_string)
+                # break
                 # print("CANONICAL LINK:", link)
                 count_appropriate_records += 1
                 # print("LANG:", language)
                 # serialized_html_page = process_html_tree(html_tree)
                 # serialized_html_page = html_to_json.convert(content_string)
-                if debug:
-                    # print(content_string)
-                    continue
                 processed_html_content = process_html(content_string)
+                print(processed_html_content)
                 if return_iterator:
                     yield processed_html_content
+                else:
+                    print(processed_html_content)
                 # print(processed_html_content)
                 # print(serialized_html_page)
                 # warc_json_pages[str(record.record_id)] = serialized_html_page
@@ -213,36 +260,27 @@ def process_warc_file(file_path: str, limit_records: int = -1, return_iterator: 
                 # http_headers[str(record.record_id)] = record_http_headers
             else:
                 languages_codes.add(language)
-    print(count_appropriate_records)
-    # with open(f'/home/skartavykh/MyProjects/media-bot/storage/warcdata/headers_{time.time()}.json', 'w') as f:
-    #     json.dump(headers, f, indent=4, ensure_ascii=False)
-    # with open(f'/home/skartavykh/MyProjects/media-bot/storage/warcdata/http_headers_{time.time()}.json', 'w') as f:
-    #     json.dump(http_headers, f, indent=4, ensure_ascii=False)
-    # with open(f'/home/skartavykh/MyProjects/media-bot/storage/json_from_html/warc_records_json_{time.time()}.json', 'w') as f:
-    #     json.dump(warc_json_pages, f, indent=4, ensure_ascii=False)
+
 
 
 def process_one_warc_file(warc_file_name: str, records: int = 1000, debug: bool = False) -> int:
-    i = 0
-    bucket_records = 10
-    items_to_add = []
     if debug:
-        for item in process_warc_file(file_path=WARC_FILES_DIR + warc_file_name, limit_records=-1, debug=True):
-            i += 1
-            # items_to_add.append(ParsedPacket(packet=item))
-            # print(item)
+        for i, item in enumerate(process_warc_file(file_path=WARC_FILES_DIR + warc_file_name,
+                                                   return_iterator=True, limit_records=-1, debug=True)):
             if i > records:
                 break
-        return 100
-
+            print(item)
+        return GeneralStatusCode.success
+    items_to_add = []
+    bucket_records = 10
     with Session(engine) as session:
         try:
-            for item in process_warc_file(file_path=WARC_FILES_DIR + warc_file_name, limit_records=-1):
+            i = 0
+            for item in process_warc_file(file_path=WARC_FILES_DIR + warc_file_name,
+                                          return_iterator=True,
+                                          limit_records=-1):
                 i += 1
                 items_to_add.append(ParsedPacket(packet=item))
-                # print(item)
-                if i > records:
-                    break
                 if i > bucket_records:
                     session.add_all(items_to_add)
                     session.commit()
@@ -251,7 +289,7 @@ def process_one_warc_file(warc_file_name: str, records: int = 1000, debug: bool 
         except KeyboardInterrupt:
             session.commit()
         session.commit()
-    return 100
+    return GeneralStatusCode.success
 
 
 def run_tasks_in_process_pool_executor(executor_for_file: Callable, dump_files: list[str]) -> None:
@@ -260,15 +298,20 @@ def run_tasks_in_process_pool_executor(executor_for_file: Callable, dump_files: 
 
 
 def run_tasks_with_multiprocessing_pool() -> None:
-    process_one_warc_file_partial = functools.partial(process_one_warc_file, records=500, debug=True)
+    process_one_warc_file_partial = functools.partial(process_one_warc_file, records=-1, debug=False)
     warc_dump_files = os.listdir(WARC_FILES_DIR)
     with Pool(processes=4) as process_pool:
-        process_pool.map(process_one_warc_file_partial, warc_dump_files)
+        process_pool.map(process_one_warc_file_partial, reversed(warc_dump_files))
 
 
 def debug_process_warc_file() -> None:
-    process_warc_file(file_path=WARC_FILES_DIR + 'CC-NEWS-20240101002957-01499.warc.gz', limit_records=10,
-                      return_iterator=False, debug=False)
+    try:
+        iterator = process_warc_file(file_path=WARC_FILES_DIR + 'CC-NEWS-20240101002957-01499.warc.gz', limit_records=-1,
+                                     return_iterator=True, debug=False)
+        for i, _ in enumerate(iterator):
+            print(i, "=" * 40)
+    except KeyboardInterrupt:
+        print("MAIN_LINKS_NOT_FOUND", MAIN_LINKS_NOT_FOUND)
 
 
 if __name__ == '__main__':
--- DataApp/storage_schemas/storage.py
+++ DataApp/storage_schemas/storage.py
@@ -61,6 +61,8 @@ class ParsedPacket(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     packet: Mapped[dict] = mapped_column(JSONB)
+    appearance_date: Mapped[datetime.datetime] = mapped_column(insert_default=func.now(), nullable=False,
+                                                               server_default=func.now())
 
 
 class Block(Base):
