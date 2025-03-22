6692891739696b6b4837bcbc86e93ead0895aaa2
sklion <sktvpx@gmail.com>
2025-03-22 11:57:09 +0300

    Event generator is edited

--- FrontendApp/app/app.py
+++ FrontendApp/app/app.py
@@ -1,7 +1,10 @@
 import asyncio
+import logging
 import random
 import time
 from contextlib import asynccontextmanager
+from typing import Iterable
+
 from sse_starlette.sse import EventSourceResponse
 import uvicorn
 from fastapi import FastAPI, Request, HTTPException
@@ -13,10 +16,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 connection_manager = ConnectionManager()
 cache_system_plugin = AsyncCacheSystemPlugin()
-
 
 EVENT_UPDATE_STATE = False
 
@@ -28,7 +29,7 @@ async def lifespan(app_: FastAPI):
     print('Shutdown procedures...')
 
 
-app = FastAPI(debug=True, lifespan=lifespan)
+app = FastAPI(debug=True, lifespan=lifespan, log_level="trace")
 
 
 @app.get("/change-frontend-state-signal")
@@ -42,39 +43,38 @@ STREAM_DELAY = 0.2
 
 class MessageInterface:
     @classmethod
-    def build_message(cls):
-        mess = {"timestamp": time.time_ns(), "test_number": random.randint(0, 1000)}
-        return mess
+    def build_messages(cls) -> Iterable:
+        return [
+            {
+                "timestamp": time.time_ns(),
+                "test_number": random.randint(0, 1000)
+            }
+        ]
+
+
+async def event_generator(request: Request, start_time: float, timeout: float, stream_delay: float):
+    while True:
+        if await request.is_disconnected() or time.time() - start_time > timeout:
+            break
+        for message in MessageInterface.build_messages():
+            yield message
+        await asyncio.sleep(stream_delay)
 
 
 @app.get('/stream')
 async def message_stream(request: Request):
-    print('start streaming...')
+    logging.info('start streaming...')
 
     _TIMEOUT = 5
     _start = time.time()
 
-    def new_messages():
-        for _ in range(10):
-            yield MessageInterface.build_message()
-
-    async def event_to_stream_generator():
-        while True:
-            if await request.is_disconnected() or time.time() - _start > _TIMEOUT:
-                break
-            message = new_messages()
-            if message:
-                yield message
-            await asyncio.sleep(STREAM_DELAY)
-
-    response = EventSourceResponse(event_to_stream_generator())
+    response = EventSourceResponse(event_generator(request, _start, _TIMEOUT, STREAM_DELAY))
     response.headers["Access-Control-Allow-Origin"] = "*"
     return response
 
 
 FRONTEND_APP_HOST = 'localhost'
 FRONTEND_APP_PORT = 8001
-
 
 storage_plugin = AsyncDatabaseStoragePlugin()
 
